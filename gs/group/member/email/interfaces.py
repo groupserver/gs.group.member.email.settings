209e317f90f46c3106a06e071f335b27e65a0875
Michael JasonSmith <mpj17@onlinegroups.net>
2013-06-03 16:29:44 -0700

    Code cleanup, helped by Nina IDE

--- gs/group/member/email/interfaces.py
+++ gs/group/member/email/interfaces.py
@@ -1,7 +1,8 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 from zope.interface import Interface
-from zope.schema import *
+from zope.schema import TextLine, Choice, List
 from Products.GSProfile.interfaces import deliveryVocab
+
 
 class IGSGroupEmailUserId(Interface):
     userId = TextLine(title=u"User Id",
@@ -9,17 +10,21 @@ class IGSGroupEmailUserId(Interface):
                       required=True
                       )
 
+
 class IGSGroupEmailDeliverySettings(Interface):
     delivery = Choice(title=u'Message Delivery',
       description=u'Your message delivery settings.',
       vocabulary=deliveryVocab)
 
+
 class IGSGroupEmailDestinationSettings(Interface):
     default_or_specific = Choice(title=u"Message Destination",
-                                 description=u"Should the message go to your default email address, or a specific one?",
+                                 description=u'Should the message go to your '
+                                     u'default email address, or a specific '
+                                     u'one?',
                                  vocabulary="DefaultOrSpecificEmailVocab",
                                  required=True)
-    
+
     destination = List(title=u'Message Destination',
                        description=u'Your message delivery settings.',
                        value_type=Choice(title=u'Email',
@@ -27,9 +32,7 @@ class IGSGroupEmailDestinationSettings(Interface):
                        unique=True,
                        required=False)
 
-class IGSGroupEmailSettings(IGSGroupEmailUserId, IGSGroupEmailDeliverySettings, IGSGroupEmailDestinationSettings):
+
+class IGSGroupEmailSettings(IGSGroupEmailUserId, IGSGroupEmailDeliverySettings,
+                            IGSGroupEmailDestinationSettings):
     pass
-   
-    
-
-
