e7cfdd928f2681afdcebbdd9626699c12727f691
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-04 10:33:00 -0700

    Adding myself as the maintainer.

--- setup.py
+++ setup.py
@@ -1,15 +1,33 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
 setup(name='gs.group.member.email.settings',
     version=version,
     description="The pages to allow the editing of group related email details",
-    long_description=open("README.txt").read() + "\n" +
-                    open(os.path.join("docs", "HISTORY.txt")).read(),
+    long_description=long_description,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
@@ -17,17 +35,19 @@ setup(name='gs.group.member.email.settings',
         "Intended Audience :: Developers",
         'License :: OSI Approved :: Zope Public License',
         "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
+        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords='user, group, member, group member, email',
     author='Richard Waid',
     author_email='richard@onlinegroups.net',
-    url='http://groupserver.org',
+    maintainer='Michael JasonSmith',
+    maintainter_email='mpj17@onlinegroups.net',
+    url='https://source.iopen.net/groupserver/gs.group.member.email.settings/',
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.member', 
+    namespace_packages=['gs', 'gs.group', 'gs.group.member',
                         'gs.group.member.email'],
     include_package_data=True,
     zip_safe=False,
